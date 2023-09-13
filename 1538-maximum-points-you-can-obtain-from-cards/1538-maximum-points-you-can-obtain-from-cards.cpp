4c85c13875b90407887e66074f4db6385a3afeba
Souvik Panda <86113322+noobsouvik99@users.noreply.github.com>
2023-09-13 22:57:24 +0530

    Time: 62 ms (26.97%), Space: 44.4 MB (9.32%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
@@ -6,19 +6,15 @@ public:
         int sum = 0;
         vector<int> leftsum(k);
         leftsum[0] = cardPoints[0];
-        cout << "Leftsum : " << leftsum[0] << endl;
         vector<int> rightsum(k);
         for(int i = 1; i < k; i++)
         {
             leftsum[i] = leftsum[i-1] + cardPoints[i];
-            cout << "Leftsum : " << leftsum[i] << endl;
         }
         rightsum[0] = cardPoints[n-1];
-        cout << "rightsum : " << rightsum[0] << endl;
         for(int i= 1; i <k; i++)
         {
             rightsum[i] = rightsum[i-1] + cardPoints[n-i-1];
-            cout << "rightsum : " << rightsum[i] << endl;
         }
         sum = max(sum, leftsum[k-1]);
         sum = max(sum, rightsum[k-1]);
@@ -27,27 +23,5 @@ public:
             sum = max(sum, leftsum[i]+rightsum[k-i-2]);
         }
         return sum;
-        /*
-        while(k--)
-        {
-            if(cardPoints[left] > cardPoints[right])
-            {
-                sum+= cardPoints[left++];
-            }
-            else
-                sum += cardPoints[right--];
-        }
-        /*int prefixsumleft = 0;
-        for(int i = 0; i <k; i++)
-        {
-            prefixsumleft += cardPoints[i];
-        }
-        int prefixsumright = 0;
-        int j = n-1;
-        while(k--)
-        {
-            prefixsumright+= cardPoints[j--];
-        }
-        return max(prefixsumleft, prefixsumright);*/
     }
 };
