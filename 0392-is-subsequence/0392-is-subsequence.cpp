372a1957a25df64ea00d8c2d5a6b8ee274aba2b7
Souvik Panda <86113322+noobsouvik99@users.noreply.github.com>
2023-09-22 19:45:40 +0530

    Time: 4 ms (10.63%), Space: 9 MB (5.02%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
-    bool isSubsequence(string s, string t) {
-        int slength = s.length();
+    int isSubs(string& s1, string& s2, int i , int j,vector<vector<int>> &t)
+    {
+        if(i == 0 || j == 0)
+            return 0;
+        if(t[i][j] != -1)
+            return t[i][j];
+        if(s1[i-1] == s2[j-1])
+            return t[i][j] = 1 + isSubs(s1,s2,i-1,j-1,t);
+        else
+            return t[i][j] = isSubs(s1,s2,i,j-1,t);
+    }
+    bool isSubsequence(string s1, string s2) {
+        /*int slength = s.length();
         int tlength = t.length();
         if(slength > tlength)
             return false;
@@ -13,6 +24,19 @@ public:
                sindex++; 
             }
         }
-        return (sindex == slength);
+        return (sindex == slength);*/
+        int m = s1.length();
+        int n = s2.length();
+        
+        // intialising dp matrix with -1
+        
+        if(m >  n)
+            return false;
+        
+        vector<vector<int>> t(m+1,vector<int> (n+1,-1));
+    
+        if(isSubs(s1,s2,m,n,t) == m)
+            return true;
+        return false;
     }
 };
