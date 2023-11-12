a4563335b3ad400757391b0a206e23c410fab0f9
Souvik Panda <86113322+noobsouvik99@users.noreply.github.com>
2023-11-12 13:17:05 +0530

    Time: 0 ms (100%), Space: 6.6 MB (52.31%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int ans = 0;
+    bool check (int row, int col, int n, vector<string>& current)
+    {
+        for(int i = 0; i < row; i++)
+        {
+            if (current[i][col] == 'Q')
+                return false;
+        }
+        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--)
+        {
+            if(current[i][j]=='Q')
+                return false;
+        }
+        for(int i=row-1,j=col+1;i>=0 && j>=0;i--,j++) 
+        {
+            if(current[i][j]=='Q')
+                return false;
+        }
+        return true;
+    }
+    void helper(vector<string>& current, int n, int row)
+    {
+        if (row == n)
+        {
+            ans++;
+            return;
+        }
+        for(int col = 0; col < n; col++)
+        {
+            if (check(row, col, n, current))
+            {
+                current[row][col] = 'Q';
+                helper(current, n , row+1);
+                current[row][col] = '.';
+            }
+        }
+    }
+    int totalNQueens(int n) {
+        vector<string> current(n, string(n,'.'));
+        helper(current, n, 0);
+        return ans;
+    }
+};
